7e9e8db51bb5a5681b9c7053785cec42191f51a4
Hao Wang <HWonner@gmail.com>
2013-06-11 16:42:11 -0600

    Finish Charge and Consume for Account

--- Yea.RuleEngine.Tests/ReviewbyHao/求源/AccountSpecs.cs
+++ Yea.RuleEngine.Tests/ReviewbyHao/求源/AccountSpecs.cs
@@ -3,19 +3,42 @@ using Machine.Specifications.AutoMocking.Rhino;
 
 namespace Yea.RuleEngine.Tests.ReviewbyHao.求源
 {
-    public class AccountSpecs:Specification<Account>
+    public class When_charge_once_to_account:Specification<Account>
     {
         Because of = () => { subject.Charge(1000); };
         It balance_should_equal_to_charged_amount = () => { subject.Balance.ShouldEqual(1000); };
     }
 
+    public class When_charge_triwce_to_account : Specification<Account> {
+        Because of = () =>
+            {
+                subject.Charge(1000);
+                subject.Charge(500);
+            };
+        It balance_should_equal_to_charged_amount = () => { subject.Balance.ShouldEqual(1500); };
+    }
+
+    public class When_charge_and_consume_to_account : Specification<Account> {
+        Because of = () =>
+        {
+            subject.Charge(1000);
+            subject.Consume(100);
+            subject.Charge(500);
+        };
+        It balance_should_equal_to_charged_amount = () => { subject.Balance.ShouldEqual(1400); };
+    }
     public class Account
     {
         public void Charge(int i)
         {
-            throw new System.NotImplementedException();
+            Balance += i;
         }
 
         public int Balance { get; private set; }
+
+        public void Consume(int i)
+        {
+            Balance -= i;
+        }
     }
 }
