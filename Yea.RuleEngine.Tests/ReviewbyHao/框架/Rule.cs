2dcccfbcaa0a389cde9f1b97fc41913aac4fbb9e
unknown <WangHao@skight.com>
2013-06-13 18:01:52 -0600

    Add Rule Specs and basic implementation

---
+++ Yea.RuleEngine.Tests/ReviewbyHao/框架/Rule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Yea.RuleEngine.Tests.ReviewbyHao.求源.框架
+{
+    public class Rule<T>
+    {
+        private Predicate<T> predicate;
+
+        public static Rule<T> New()
+        {
+            return new Rule<T>();
+        }
+
+        private Action<T> action;
+
+        public static Rule<T> When(Predicate<T> predicate)
+        {
+            return new Rule<T>();
+        }
+
+        public Rule<T> when(Predicate<T> predicate)
+        {
+            this.predicate = predicate;
+            return this;
+        }
+
+        public Rule<T> then(Action<T> action)
+        {
+            this.action = action;
+            return this;
+        }
+
+        public void apply(T target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Yea.RuleEngine.Tests/ReviewbyHao/框架/RuleSpecs.cs
@@ -0,0 +1,37 @@
+﻿using Machine.Specifications;
+
+namespace Yea.RuleEngine.Tests.ReviewbyHao.求源.框架
+{
+    public class For_a_rule_against_mock
+    {
+         
+
+        private Establish context = () =>
+                                 {
+                                     subject = Rule<Mock>
+                                         .New()
+                                         .when(x => x.Condition == 1)
+                                         .then(x => x.Result = "1");
+                                 };
+
+
+        protected static Rule<Mock> subject;
+        protected static Mock mock; 
+
+        protected class Mock
+        {
+            public int Condition { get; set; }
+            public string Result { get; set; }
+        }
+    }
+
+    public class when_condition_meet_1:For_a_rule_against_mock
+    {
+        private Establish context = () => mock = new Mock {Condition = 1};
+        private Because of = () => subject.apply(mock);
+        private It result_should_set_to_1 = () => mock.Result.ShouldEqual("1");
+
+    }
+
+
+}
