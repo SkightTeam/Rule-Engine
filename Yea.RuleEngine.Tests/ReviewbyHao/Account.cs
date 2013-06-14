a4fa2fdddcc29be3f21c31ba71177d04318fddf6
unknown <WangHao@skight.com>
2013-06-13 22:16:42 -0600

    Apply rule to account

--- Yea.RuleEngine.Tests/ReviewbyHao/Account.cs
+++ Yea.RuleEngine.Tests/ReviewbyHao/Account.cs
@@ -1,7 +1,21 @@
+using Yea.RuleEngine.Tests.ReviewbyHao.求源.框架;
+
 namespace Yea.RuleEngine.Tests.ReviewbyHao.求源
 {
     public class Account
     {
+        private Rules<Account> membership_rule;
+
+        public Account()
+        {
+            membership_rule =
+                Rules<Account>
+                    .when(x => x.ChargeAmount < 100).then(x => x.Grade = Grade.Regular)
+                    .when(x => x.ChargeAmount >= 100 && x.ChargeAmount < 1000).then(x => x.Grade = Grade.Silver)
+                    .when(x => x.ChargeAmount > 1000).then(x => x.Grade = Grade.Gold)
+                    .done();
+        }
+
         public int Balance { get; private set; }
         public int ChargeAmount { get; set; }
         public virtual Grade Grade { get; set; }
@@ -26,6 +40,8 @@ namespace Yea.RuleEngine.Tests.ReviewbyHao.求源
         {
             ChargeAmount += i;
             Balance += i;
+
+            membership_rule.apply(this);
         }
         
         public void Consume(int i)
